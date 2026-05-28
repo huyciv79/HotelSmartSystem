9ba0c5acbb7776215ae5974eb45aed84c2e7d797
huyciv79 <giahuy97531@gmail.com>
2026-05-28 18:01:40 +0700

    init project

---
+++ hotel-smart-booking-backend/src/main/java/com/example/hotelsmartbookingbackend/HotelSmartBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelsmartbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSmartBookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelSmartBookingBackendApplication.class, args);
+    }
+
+}
---
+++ hotel-smart-booking-backend/src/test/java/com/example/hotelsmartbookingbackend/HotelSmartBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelsmartbookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSmartBookingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
